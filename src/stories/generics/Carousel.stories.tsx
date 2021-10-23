ab46c2e0ef1d444a920b0b69578ae863650419c5
gabreiljofre <gabrielgoj2@gmail.com>
2021-10-23 16:42:54 -0300

    feat(card) add carousel generic component and stories

---
+++ src/stories/generics/Carousel.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Carousel } from '@components/generics/carousel';
+import { ChevronLeft } from '@components/generics/icons/chevronLeft';
+import { ChevronRight } from '@components/generics/icons/chevronRight';
+
+export default {
+  title: 'generics/Carousel',
+  component: Carousel,
+} as ComponentMeta<typeof Carousel>;
+
+const Template: ComponentStory<typeof Carousel> = (args) => (
+  <Carousel {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  images: [
+    'https://via.placeholder.com/269x143?text=1',
+    'https://via.placeholder.com/269x143?text=2',
+    'https://via.placeholder.com/269x143?text=3',
+  ],
+  chevronRight: <ChevronRight />,
+  chevronLeft: <ChevronLeft />,
+};
