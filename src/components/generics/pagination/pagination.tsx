43ef0a04a9672419e1a1c415d32881d7da636dda
josueneneve <josue.neneve@hotmail.com>
2021-11-23 18:01:25 -0300

    refactor: added options in the pagination.

--- src/components/generics/pagination/pagination.tsx
+++ src/components/generics/pagination/pagination.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 import { Dropdown } from '../dropdown';
 
-import { PaginationProps } from './pagination.type';
+import { options } from '../dropdown/dropdown.options';
 
 import * as S from './pagination.style';
 
-const Pagination = ({ items }: PaginationProps): React.ReactElement => {
+const Pagination = (): React.ReactElement => {
   return (
     <S.Container>
       <S.Label>Página:</S.Label>
       <S.Wrapper>
-        <Dropdown options={items} />
+        <Dropdown options={options.pagination} />
       </S.Wrapper>
-      <S.Number>de {items?.length}</S.Number>
+      <S.Number>de {options.pagination.length}</S.Number>
     </S.Container>
   );
 };
