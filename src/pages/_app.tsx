5e8e6c9fb5becce8c75141ef65951bdb32745851
Natan Souza <natan.souza@bolttech.io>
2021-11-23 20:25:30 +0000

    fix: persistgate to async our storage.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,35 +8,39 @@ import getTheme from '@theme/theme';
 
 import { store } from '@store/index';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { BreadCrumbProvider } from '@hooks/breadcrumb';
+import { getPersistor } from '@rematch/persist';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={getTheme()}>
-        <GlobalStyle theme={getTheme()} />
-        <BreadCrumbProvider>
-          <Head>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=UA-54516992-1`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
+      <PersistGate loading={null} persistor={getPersistor()}>
+        <ThemeProvider theme={getTheme()}>
+          <GlobalStyle theme={getTheme()} />
+          <BreadCrumbProvider>
+            <Head>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=UA-54516992-1`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
 
                   gtag('config', 'UA-54516992-1');
                 `,
-              }}
-            />
-          </Head>
-          <Component {...pageProps} />
-        </BreadCrumbProvider>
-      </ThemeProvider>
+                }}
+              />
+            </Head>
+            <Component {...pageProps} />
+          </BreadCrumbProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
